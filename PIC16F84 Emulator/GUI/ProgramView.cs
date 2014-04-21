d923e7233dee8b4d71dbbe5997bc52945d263abc
Th3Tw0 <Th3Tw0@gmx.net>
2014-04-21 22:48:16 +0200

    back end for listing viewer added

---
+++ PIC16F84 Emulator/GUI/ProgramView.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PIC16F84_Emulator.GUI
+{
+    public class ProgramView
+    {
+        private const int NO_ADDRESS_VALUE = -1;
+
+        private int linesOfCode;
+        public List<string> source;
+        private short[] address;
+
+        public ProgramView(string pathOfProgramListing)
+        {
+            string[] lines = System.IO.File.ReadAllLines(pathOfProgramListing);
+            linesOfCode = lines.Length;
+            source = new List<string>();
+            address = new short[linesOfCode];
+            string temp = "";
+            string tempAddress = "";
+            for (int i = 0; i < linesOfCode; i++)
+            {
+                temp = lines[i];
+                temp = temp.Remove(0, 27);
+                source.Add(temp);
+
+                tempAddress = lines[i].Substring(0, 4);
+                tempAddress = tempAddress.Trim();
+                if (tempAddress != "")
+                {
+                    address[i] = Int16.Parse(tempAddress, System.Globalization.NumberStyles.HexNumber);
+                }
+                else
+                {
+                    address[i] = NO_ADDRESS_VALUE;
+                }
+            }
+        }
+
+
+        public void print() {
+            for (int i = 0; i < linesOfCode; i++)
+            {
+                System.Console.WriteLine(source[i] + "|||" + address[i]);
+            }
+        }
+        
+    }
+}
