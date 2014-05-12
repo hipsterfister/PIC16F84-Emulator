202fd1065625756993df9df897ec1f7502f8f27a
hipsterfister <weissnicht05@web.de>
2014-05-12 12:06:55 +0200

    bug fix

--- PIC16F84 Emulator/PIC/Operations/ArithmeticOperation.cs
+++ PIC16F84 Emulator/PIC/Operations/ArithmeticOperation.cs
@@ -92,7 +92,7 @@ namespace PIC16F84_Emulator.PIC.Operations
             }
 
             registerFileMap.updateZeroFlag(result == 0);
-            registerFileMap.updateDigitCarry(((arg1 % 0x10) + ((~arg2+1) % 0x10)) > 0xF); // meaning of digit carry bit is inverted for subtraction (like the carry bit)
+            registerFileMap.updateDigitCarry(((arg1 % 0x10) + ((byte)((~arg2)+1) % 0x10)) > 0xF); // meaning of digit carry bit is inverted for subtraction (like the carry bit)
 
             registerFileMap.Set(result, targetAddress);
         }
