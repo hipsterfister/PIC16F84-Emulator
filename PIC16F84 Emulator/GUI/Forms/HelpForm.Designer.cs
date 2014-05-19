de362a4ff29792d792500d1940f039f599f6d6b7
hipsterfister <weissnicht05@web.de>
2014-05-19 08:59:52 +0200

    fix derp.htm

--- PIC16F84 Emulator/GUI/Forms/HelpForm.Designer.cs
+++ PIC16F84 Emulator/GUI/Forms/HelpForm.Designer.cs
@@ -39,7 +39,7 @@
             this.webBrowser1.Name = "webBrowser1";
             this.webBrowser1.Size = new System.Drawing.Size(479, 562);
             this.webBrowser1.TabIndex = 0;
-            this.webBrowser1.Url = new System.Uri(System.Environment.CurrentDirectory + "\\..\\..\\Anleitung.htm", System.UriKind.Absolute);
+            this.webBrowser1.Url = new System.Uri(System.Environment.CurrentDirectory + "\\Anleitung.htm", System.UriKind.Absolute);
             // 
             // HelpForm
             // 
